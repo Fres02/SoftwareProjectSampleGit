55f05d9461c54dd5f731488956d173518ad8c33e
Fres02 <prabhanijayaweera@gmail.com>
2024-10-18 15:43:25 +0530

    Animal dog

--- animal.py
+++ animal.py
@@ -2,9 +2,17 @@ import sys
 
 def default():
     print('Hello')
+       
+def dog():
+    print("Baw Baw")
 
 def main():
-    default()
-    
+    if sys.argv[1]=='dog':
+        dog()
+    else:
+        default()
+        
 if __name__ =='__main__':
     main()
+    
+
